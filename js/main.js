ad55831495dcad3c46491e8f7a06030e71906dc9
harakiriOP <salvadorhidalgo100@gmail.com>
2025-01-17 13:02:25 -0500

    Update: menubcc add

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     element.addEventListener("click", function (event) {
       console.log(element.classList.contains("carta-bcc"))
       if (element.classList.contains("carta-bcc") || element.closest(".carousel-item").classList.contains("active")) {
-        window.open("https://drive.google.com/file/d/1O9owhMXwaZF-gsmtU19T09w9BWFFe5Hb/view?usp=drivesdk", "_blank");
+        window.open("img/menubcc.pdf", "_blank");
       }
       else {
         event.preventDefault();
